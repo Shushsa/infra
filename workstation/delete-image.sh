e4ec758a221286997c3380a68fe9f766e6835ce0
asmodat <asmodat@gmail.com>
2020-05-22 17:03:41 +0200

    fix delete and garbage collect

--- workstation/delete-image.sh
+++ workstation/delete-image.sh
@@ -18,13 +18,17 @@ IMAGE_TAG=$3
 [ -z "$IMAGE_TAG" ] && IMAGE_TAG="latest"
 
 KIRA_SETUP_FILE="$KIRA_SETUP/$IMAGE_NAME-$IMAGE_TAG"
+IMAGE=$(docker images --format '{{.Repository}}:{{.Tag}}' --filter=reference="${IMAGE_NAME}:*" || echo "none")
+REGISTRY_IMAGE=$(docker images --format '{{.Repository}}:{{.Tag}}' --filter=reference="${KIRA_REGISTRY}/${IMAGE_NAME}" || echo "none")
 
 echo "------------------------------------------------"
 echo "|         STARTED: DELETE IMAGE v0.0.1         |"
 echo "------------------------------------------------"
-echo "|   WORKING DIR: $IMAGE_DIR"
-echo "|    IMAGE NAME: $IMAGE_NAME"
-echo "|     IMAGE TAG: $IMAGE_TAG"
+echo "|    WORKING DIR: $IMAGE_DIR"
+echo "|     IMAGE NAME: $IMAGE_NAME"
+echo "|      IMAGE TAG: $IMAGE_TAG"
+echo "|          IMAGE: $IMAGE"
+echo "| REGISTRY IMAGE: $REGISTRY_IMAGE"
 echo "------------------------------------------------"
 
 cd $IMAGE_DIR
@@ -32,12 +36,12 @@ cd $IMAGE_DIR
 rm -fv $KIRA_SETUP_FILE
 docker images -f "dangling=true" -q 
 docker images | grep "<none>" | awk '{print $3}' | xargs sudo docker rmi -f || echo "Likely detected child imaged dependencies"
-docker rmi -f $(docker images --format '{{.Repository}}:{{.Tag}}' --filter=reference="${IMAGE_NAME}:*") || echo "Image not found"
-docker rmi -f $(docker images --format '{{.Repository}}:{{.Tag}}' --filter=reference="${KIRA_REGISTRY}/${IMAGE_NAME}") || echo "Image not found"
+[ "$IMAGE" != "none" ] && docker rmi -f $IMAGE || echo "Image not found"
+[ "$REGISTRY_IMAGE" != "none" ] &&  rmi -f $REGISTRY_IMAGE || echo "Image not found"
 
 # ensure registry cleanup
-docker exec -it registry sh -c "rm -rfv /var/lib/registry/docker/registry/v2/repositories/${IMAGE_NAME}"
-docker exec -it registry bin/registry garbage-collect /etc/docker/registry/config.yml -m
+docker exec -it registry sh -c "rm -rfv /var/lib/registry/docker/registry/v2/repositories/${IMAGE_NAME}" || echo "Imgae was not present in the registry"
+docker exec -it registry bin/registry garbage-collect /etc/docker/registry/config.yml -m  || echo "Failed to collect registry garbage"
 docker exec -it registry sh -c "reboot" || echo "Docker Registry Reboot" && sleep 1
 
 echo "------------------------------------------------"
