fc747302daf1c972cb0ad3832352260f944ad0f0
asmodat <asmodat@gmail.com>
2020-06-01 22:01:49 +0200

    Retry google chrome install

--- workstation/setup/chrome.sh
+++ workstation/setup/chrome.sh
@@ -12,7 +12,11 @@ KIRA_SETUP_CHROME="$KIRA_SETUP/chrome-v0.0.1"
 if [ ! -f "$KIRA_SETUP_CHROME" ] ; then
     echo "INFO: Installing Google Chrome..."
     apt-get update -y --fix-missing
-    apt install google-chrome-stable -y
+    apt install google-chrome-stable -y || FAILED="True"
+    [ "$FAILED" == "True" ] && \
+        echo "Failed to install google chrome, retry in 5 seconds..." && \
+        sleep 5 && \
+        apt install google-chrome-stable -y
     google-chrome --version
     touch $KIRA_SETUP_CHROME
 else
