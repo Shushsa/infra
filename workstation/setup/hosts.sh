95baa4e5ea2e69179830ec4548278425c8dd4501
dev <dev@local>
2020-06-10 06:28:19 -0700

    [10/06/2020 06:28:19] Fix host block skip

--- workstation/setup/hosts.sh
+++ workstation/setup/hosts.sh
@@ -9,7 +9,7 @@ source $ETC_PROFILE &> /dev/null
 if [ "$DEBUG_MODE" == "True" ] ; then set -x ; else set +x ; fi
 
 KIRA_SETUP_HOSTS="$KIRA_SETUP/hosts-v0.0.1-$KIRA_REGISTRY_NAME" 
-if [ ! -f "$KIRA_SETUP_CERTS" ] ; then
+if [ ! -f "$KIRA_SETUP_HOSTS" ] ; then
     echo "INFO: Setting up default hosts..."
     HOSTS_PATH="/etc/hosts"
     CDHelper text lineswap --insert="127.0.0.1 localhost $KIRA_REGISTRY_NAME" --prefix="127.0.0.1" --path=$HOSTS_PATH --append-if-found-not=True --silent=$SILENT_MODE
