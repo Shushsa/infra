e1922d724df233d74ee09d9426b50a34a098c70c
dev <dev@local>
2020-06-10 06:19:48 -0700

    [10/06/2020 06:19:48] Ensure host is present

--- workstation/setup/hosts.sh
+++ workstation/setup/hosts.sh
@@ -12,7 +12,7 @@ KIRA_SETUP_HOSTS="$KIRA_SETUP/hosts-v0.0.1-$KIRA_REGISTRY_NAME"
 if [ ! -f "$KIRA_SETUP_CERTS" ] ; then
     echo "INFO: Setting up default hosts..."
     HOSTS_PATH="/etc/hosts"
-    CDHelper text lineswap --insert="127.0.0.1 localhost $KIRA_REGISTRY_NAME" --contains-not="$KIRA_REGISTRY_NAME" --prefix="127.0.0.1" --path=$HOSTS_PATH --append-if-found-not=True --silent=$SILENT_MODE
+    CDHelper text lineswap --insert="127.0.0.1 localhost $KIRA_REGISTRY_NAME" --prefix="127.0.0.1" --path=$HOSTS_PATH --append-if-found-not=True --silent=$SILENT_MODE
     touch $KIRA_SETUP_HOSTS
 else
     echo "INFO: Default host names were already defined"
