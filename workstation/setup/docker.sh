de10c276199840122821edb3f66be92fd2d1453c
asmodat <asmodat@gmail.com>
2020-06-12 19:35:40 +0200

    update docker on failure

--- workstation/setup/docker.sh
+++ workstation/setup/docker.sh
@@ -8,12 +8,13 @@ ETC_PROFILE="/etc/profile"
 source $ETC_PROFILE &> /dev/null
 if [ "$DEBUG_MODE" == "True" ] ; then set -x ; else set +x ; fi
 
-KIRA_SETUP_DOCKER="$KIRA_SETUP/docker-v0.0.1" 
-if [ ! -f "$KIRA_SETUP_DOCKER" ] ; then
+VERSION=$(docker -v || echo "Error")
+KIRA_SETUP_DOCKER="$KIRA_SETUP/docker-v0.0.2" 
+if [ ! -f "$KIRA_SETUP_DOCKER" ] || [ "$VERSION" == "Error" ] ; then
     echo "INFO: Installing Docker..."
     apt-get update
     apt install docker.io -y
-    systemctl2 enable --now docker
+    systemctl enable --now docker
     docker -v
     touch $KIRA_SETUP_DOCKER
 else
