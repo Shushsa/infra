11e21009f7005b495650cdc54a4ef2a002577656
dev <dev@local>
2020-06-16 13:22:47 -0700

    [16/06/2020 13:22:47] Healtcheck more gracefull restarts

--- docker/validator/container/healthcheck.sh
+++ docker/validator/container/healthcheck.sh
@@ -28,10 +28,18 @@ fi
 
 RPC_STATUS="$(curl 127.0.0.1:$RPC_PROXY_PORT/status 2>/dev/null)" || RPC_STATUS="{}"
 RPC_CATCHING_UP="$(echo $RPC_STATUS | jq -r '.result.sync_info.catching_up')" || RPC_CATCHING_UP="true"
-STATUS_NGINX="$(systemctl2 is-active nginx.service)" || STATUS_RELAYER="unknown"
-STATUS_SEKAI="$(systemctl2 is-active sekaid.service)" || STATUS_SEKAI="unknown"
-STATUS_LCD="$(systemctl2 is-active lcd.service)" || STATUS_LCD="unknown"
-STATUS_FAUCET="$(systemctl2 is-active faucet.service)" || STATUS_FAUCET="unknown"
+
+set +e
+STATUS_NGINX="$(systemctl2 is-active nginx.service)"
+STATUS_SEKAI="$(systemctl2 is-active sekaid.service)"
+STATUS_LCD="$(systemctl2 is-active lcd.service)"
+STATUS_FAUCET="$(systemctl2 is-active faucet.service)"
+set -e
+
+[ -z "$STATUS_NGINX" ] && STATUS_NGINX="unknown"
+[ -z "$STATUS_SEKAI" ] && STATUS_SEKAI="unknown"
+[ -z "$STATUS_LCD" ] && STATUS_LCD="unknown"
+[ -z "$STATUS_FAUCET" ] && STATUS_FAUCET="unknown"
 
 BLOCK_HEIGHT_FILE="$SELF_LOGS/latest_block_height.txt" && touch $BLOCK_HEIGHT_FILE
 HEIGHT=$(sekaicli status 2>/dev/null | jq -r '.sync_info.latest_block_height' 2>/dev/null | xargs || echo "")
@@ -63,25 +71,39 @@ if [ "$BLOCK_CHANGED" == "False" ] || [ "${STATUS_SEKAI}" != "active" ] || [ "${
 
     if [ "${STATUS_SEKAI}" != "active" ] ; then
         echo ">> Sekai log:"
-        tail -n 100 /var/log/journal/sekaid.service.log || true
+        tail -n 100 /var/log/journal/sekaid.service.log || True
+
+        systemctl2 stop lcd || echo "ERROR: Failed to stop lcd service"
+        systemctl2 stop nginx || echo "ERROR: Failed to stop nginx service"
         systemctl2 stop sekaid || echo "ERROR: Failed to stop sekaid service"
+
+        kill $(ps aux | grep '[s]ekaicli' | awk '{print $2}') || echo "ERROR: Failed to kill sekaicli"
+        kill $(ps aux | grep '[n]ginx' | awk '{print $2}') || echo "ERROR: Failed to kill nginx"
+        kill $(ps aux | grep '[s]ekaid' | awk '{print $2}') || echo "ERROR: Failed to kill sekaid"
+
         systemctl2 start sekaid || systemctl2 status sekaid.service || echo "ERROR: Failed to re-start sekaid service" || true
     fi
 
-    if [ "${STATUS_NGINX}" != "active" ] || [ "${STATUS_LCD}" != "active" ] ; then
+    if [ "${STATUS_LCD}" != "active" ] || [ "${STATUS_SEKAI}" != "active" ] ; then
+        echo ">> LCD log:"
+        tail -n 100 /var/log/journal/lcd.service.log || true
+
+        systemctl2 stop lcd || echo "ERROR: Failed to stop lcd service"
+        systemctl2 stop nginx || echo "ERROR: Failed to stop nginx service"
+
+        kill $(ps aux | grep '[s]ekaicli' | awk '{print $2}') || echo "ERROR: Failed to kill sekaicli"
+        kill $(ps aux | grep '[n]ginx' | awk '{print $2}') || echo "ERROR: Failed to kill nginx"
+
+        systemctl2 start lcd || systemctl2 status lcd.service || echo "ERROR: Failed to re-start lcd service" || true
+    fi
+
+    if [ "${STATUS_NGINX}" != "active" ] || [ "${STATUS_LCD}" != "active" ] || [ "${STATUS_SEKAI}" != "active" ] ; then
         echo ">> NGINX log:"
         tail -n 100 /var/log/journal/nginx.service.log || true
         nginx -t || echo "ERROR: Failed to check nginx config"
         systemctl2 stop nginx || echo "ERROR: Failed to stop nginx service"
         kill $(ps aux | grep '[n]ginx' | awk '{print $2}') || echo "ERROR: Failed to kill nginx"
         systemctl2 start nginx || systemctl2 status nginx.service || echo "ERROR: Failed to re-start nginx service" || true
-    fi
-
-    if [ "${STATUS_LCD}" != "active" ]  ; then
-        echo ">> LCD log:"
-        tail -n 100 /var/log/journal/lcd.service.log || true
-        systemctl2 stop lcd || echo "ERROR: Failed to stop lcd service"
-        systemctl2 start lcd || systemctl2 status lcd.service || echo "ERROR: Failed to re-start lcd service" || true
     fi
 
     #if [ "${STATUS_FAUCET}" != "active" ]  ; then
